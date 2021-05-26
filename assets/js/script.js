be9fb8ff87f64106cfc5a2770470d9c1281c5dab
Christopher Currin <chris.crisur@gmail.com>
2021-05-26 23:43:00 +0200

    update slick slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,21 +113,26 @@ $(window).on('load', function () {
 	$('.faculty-logo-slider').slick({
 		infinite: true,
 		// speed: 100, 
-		slidesToShow: 8,
-		slidesToScroll: 4,
+		slidesToShow: 5,
+		rows: 3,
+		slidesPerRow: 1,
+		slidesToScroll: 3,
 		swipeToSlide: true,
 		autoplay: true,
 		autoplaySpeed: 1000,
 		dots: true,
 		arrows: true,
+		centerMode: false,
 		pauseOnHover: false,
 		nextArrow: '<button type="button" class="slick-next btn btn-outline-dark">></button>',
 		prevArrow: '<button type="button" class="slick-next btn btn-outline-dark"><</button>',
 		responsive: [{
 				breakpoint: 1024,
 				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 1
+					slidesToShow: 2,
+					rows: 2,
+					slidesPerRow: 2,
+					slidesToScroll: 4,
 				}
 			},
 			{
@@ -141,14 +146,18 @@ $(window).on('load', function () {
 				breakpoint: 480,
 				settings: {
 					slidesToShow: 2,
+					slidesPerRow: 1,
 					slidesToScroll: 1
 				}
 			},
 			{
 				breakpoint: 400,
 				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
+					slidesToShow: 2,
+					slidesPerRow: 1,
+					rows: 3,
+					// slidesPerRow: 1, 
+					// slidesToScroll: 1
 				}
 			}
 		]
